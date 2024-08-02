d9a3ac6c4a0947499320da89a51331382d9597ea
Gabriel de Melo Berberian <gaberberi@gmail.com>
2024-08-01 22:59:59 -0300

    Initial commit

---
+++ json_processor.py
@@ -0,0 +1,36 @@
+# Process json in order to remove specified attributes
+def process_json(data,
+                 remove_mission=True,
+                 remove_responsibilities=True,
+                 remove_responsible=True,
+                 remove_collaborators=True,
+                 remove_channels=True):
+    if isinstance(data, dict):
+        if remove_mission and "mission" in data:
+            del data["mission"]
+        if remove_responsibilities and "responsibilities" in data:
+            del data["responsibilities"]
+        if remove_responsible and "responsible" in data:
+            del data["responsible"]
+        if remove_channels and "channels" in data:
+            del data["channels"]
+        if remove_collaborators and "collaborators" in data:
+            del data["collaborators"]
+        for key, value in data.items():
+            process_json(
+                value,
+                remove_mission,
+                remove_responsibilities,
+                remove_responsible,
+                remove_collaborators,
+                remove_channels)
+    elif isinstance(data, list):
+        for item in data:
+            process_json(
+                item,
+                remove_mission,
+                remove_responsibilities,
+                remove_responsible,
+                remove_collaborators,
+                remove_channels)
+    return data
---
+++ main.py
@@ -0,0 +1,52 @@
+import json
+import argparse
+from print_tree import to_tree
+from json_processor import process_json
+
+
+def main(
+        input_path,
+        output_path,
+        keep_mission,
+        keep_responsibilities,
+        keep_responsible,
+        keep_collaborators,
+        keep_channels):
+    with open(input_path, "r") as infile:
+        data = json.load(infile)
+
+    # Process JSON to remove specified attributes
+    data = process_json(data,
+                        not keep_mission,
+                        not keep_responsibilities,
+                        not keep_responsible,
+                        not keep_collaborators,
+                        not keep_channels)
+
+    # Parse JSON to tree format
+    result = to_tree(data)
+
+    with open(output_path, "w") as outfile:
+        for line in result:
+            outfile.write(line + "\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process JSON and print it in tree format")
+    parser.add_argument("-i", "--input", required=True, help="Input JSON file path")
+    parser.add_argument("-o", "--output", required=True, help="Output file path")
+    parser.add_argument("-m", "--mission", action="store_true", help="Keep 'mission' attributes")
+    parser.add_argument("-r", "--responsibilities", action="store_true", help="Keep 'responsibilities' attributes")
+    parser.add_argument("-R", "--responsible", action="store_true", help="Keep 'responsible' attributes")
+    parser.add_argument("-C", "--collaborators", action="store_true", help="Keep 'collaborators' attributes")
+    parser.add_argument("-c", "--channels", action="store_true", help="Keep 'channels' attributes")
+    args = parser.parse_args()
+
+    main(
+        args.input,
+        args.output,
+        args.mission,
+        args.responsibilities,
+        args.responsible,
+        args.collaborators,
+        args.channels)
